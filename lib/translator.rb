5863969312461c7a60389f61819e0602ca364c57
nickie kane <kameo712@gmail.com>
2020-01-06 03:50:29 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -5,10 +5,10 @@ require 'yaml'
 def load_library(path)
   # code goes here
   emoticons = YAML.load_file(path)
-  e_hash = {:get_info=>{}, :get_emoticon=>{}}
+  e_hash = {:get_meaning=>{}, :get_emoticon=>{}}
   emoticons.each do |tra, jap|
     e_hash[:get_emoticon][jap.first] = jap.last 
-    e_hash[:get_info][jap.last] = tra
+    e_hash[:get_meaning][jap.last] = tra
   end
   return e_hash
 end
@@ -16,7 +16,7 @@ end
 def get_japanese_emoticon(path, emoticons)
   # code goes here
   e_hash = load_library(path)
-  tra = e_hash[:g_emo][emoticons]
+  tra = e_hash[:get_emoticon][emoticons]
   if tra == nil 
     tra = "Sorry, that emoticon was not found"
   end
@@ -26,7 +26,7 @@ end
 def get_english_meaning(path, emoticon)
   # code goes here
   e_hash = load_library(path)
-  e_me = e_hash[:get_info][emoticon]
+  e_me = e_hash[:get_meaning][emoticon]
   if e_me == nil 
     e_me = "Sorry, that emoticon was not found"
   end
