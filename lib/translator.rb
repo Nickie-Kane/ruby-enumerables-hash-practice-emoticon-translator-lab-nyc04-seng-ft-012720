f85aa12609b17a8ffd106fb77c9f294d9d11885d
nickie kane <kameo712@gmail.com>
2020-01-06 03:37:47 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,11 +1,20 @@
+require 'pry'
+require 'yaml'
 # require modules here
 
-def load_library
+def load_library(path)
   # code goes here
+  emoticons = YAML.load_file(path)
+  e_hash = {:get_info=>{}, :g_emo=>{}}
+  emoticons.each do |tra, jap|
+    e_hash[:g_emo][jap.first] = jap.last 
+    e_hash[:get_info][jap.last]
+  end
 end
 
 def get_japanese_emoticon
   # code goes here
+  
 end
 
 def get_english_meaning
