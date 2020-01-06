9799c03c44d596e1b5d4b0ed554358b705e492d6
nickie kane <kameo712@gmail.com>
2020-01-06 03:40:30 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -8,12 +8,14 @@ def load_library(path)
   e_hash = {:get_info=>{}, :g_emo=>{}}
   emoticons.each do |tra, jap|
     e_hash[:g_emo][jap.first] = jap.last 
-    e_hash[:get_info][jap.last]
+    e_hash[:get_info][jap.last] = tra
   end
+  return e_hash
 end
 
 def get_japanese_emoticon
   # code goes here
+  
   
 end
 
