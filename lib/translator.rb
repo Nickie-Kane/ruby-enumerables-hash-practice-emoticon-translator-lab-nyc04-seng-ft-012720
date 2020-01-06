a5b597d61fa26df0136f7b62d78f3e56962c3306
nickie kane <kameo712@gmail.com>
2020-01-06 03:49:57 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -5,9 +5,9 @@ require 'yaml'
 def load_library(path)
   # code goes here
   emoticons = YAML.load_file(path)
-  e_hash = {:get_info=>{}, :g_emo=>{}}
+  e_hash = {:get_info=>{}, :get_emoticon=>{}}
   emoticons.each do |tra, jap|
-    e_hash[:g_emo][jap.first] = jap.last 
+    e_hash[:get_emoticon][jap.first] = jap.last 
     e_hash[:get_info][jap.last] = tra
   end
   return e_hash
